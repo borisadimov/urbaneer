77fbde6e35a3b562532a8a730c9e6a67e4e502a1
Boris Adimov <boris@adimov.ru>
2015-11-21 01:26:03 +0300

    upd

--- javascripts/app.js
+++ javascripts/app.js
@@ -193,8 +193,8 @@ isiPad = false;
 
 ipadCheck = function() {
   isiPad = navigator.userAgent.match(/iPad/i) !== null;
-  if (isiPad) {
-    return $('body').addClass('is-ipad');
+  if (!isiPad) {
+    return $('body').addClass('not-ipad');
   }
 };
 
@@ -217,8 +217,13 @@ initScrollMaigic = (function(_this) {
     require('scrollmagic/custom_scrollbar')(controller);
     require('scrollmagic/earth_section')(controller);
     require('scrollmagic/timeline_section')(controller);
-    require('scrollmagic/about_section')(controller);
-    require('scrollmagic/get_started_section')(controller);
+    if (!isiPad) {
+      require('scrollmagic/about_section')(controller);
+      require('scrollmagic/get_started_section')(controller);
+    } else {
+      require('scrollmagic/about_section_ipad')(controller);
+      require('scrollmagic/get_started_section_ipad')(controller);
+    }
     require('scrollmagic/get_smarter_section')(controller);
     scene_footer = new ScrollMagic.Scene({
       triggerElement: ".footer",
@@ -475,6 +480,315 @@ module.exports = function(controller) {
   }));
   scene_what_we_do_tips = new ScrollMagic.Scene({
     triggerElement: ".about_us",
+    duration: 1350,
+    offset: 900,
+    tweenChanges: true
+  }).triggerHook(0.1).setPin('.about_us .container.long').setTween(what_we_do_tips_tween).addTo(controller);
+  jessica_tween = new TimelineMax().add([
+    TweenMax.to($('.guidelines .ln3'), 0.1, {
+      'height': '550px',
+      delay: 0.4
+    }), TweenMax.to($('.stories .story1'), 4, {
+      'transform': 'translate3D(0,-900px,0)'
+    }), TweenMax.to($('.stories .story2'), 2.5, {
+      'transform': 'translate3D(0,-900px,0)'
+    }), TweenMax.to($('.stories .story3'), 2, {
+      'transform': 'translate3D(0,-1100px,0)',
+      delay: 0.1
+    }), TweenMax.to($('.stories .story4'), 3, {
+      'transform': 'translate3D(0,-1100px,0)',
+      delay: 0.3
+    }), TweenMax.to($('.stories .story5'), 3.5, {
+      'transform': 'translate3D(0,-1200px,0)',
+      delay: 0.6
+    }), TweenMax.to($('.stories .story1 .quote'), 4.5, {
+      'transform': 'translate3D(0,-50px,0)'
+    }), TweenMax.to($('.stories .story2 .quote'), 5, {
+      'transform': 'translate3D(0,-200px,0)'
+    }), TweenMax.to($('.stories .story3 .quote'), 5, {
+      'transform': 'translate3D(0, 100px,0)',
+      delay: 0.1
+    }), TweenMax.to($('.stories .story4 .quote'), 4.3, {
+      'transform': 'translate3D(0,-250px,0)',
+      delay: 0.3
+    }), TweenMax.to($('.stories .story5 .quote'), 4.8, {
+      'transform': 'translate3D(0,-200px,0)',
+      delay: 0.6
+    }), TweenMax.to($('.guidelines .ln4'), 2, {
+      'height': '1132px',
+      delay: 1
+    })
+  ]).add(TweenMax.to($('.guidelines .ln4_1'), 0.2, {
+    'width': '940px'
+  }));
+  scene_jessica = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 1400,
+    offset: 2400
+  }).triggerHook(0.3).on('start', function(e) {
+    if (e.target.controller().info("scrollDirection") === "FORWARD") {
+      return $('.ln2').addClass('complete');
+    } else {
+      return $('.ln2').removeClass('complete');
+    }
+  }).setTween(jessica_tween).addTo(controller);
+  how_we_do_it_tween = new TimelineMax().add(TweenMax.to($('.guidelines .ln5'), 2, {
+    'height': '330px'
+  })).add(TweenMax.to($('.how.fadein'), 1, {
+    'opacity': '0.9999'
+  }));
+  scene_how_we_do_it = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 400,
+    offset: 3950
+  }).triggerHook(0.2).setTween(how_we_do_it_tween).addTo(controller);
+  how_we_do_it_icons_tween = new TimelineMax().add([
+    TweenMax.to($('.guidelines .ln6'), 0.2, {
+      'width': '130px'
+    }), TweenMax.fromTo('.culture', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln6'), 0.5, {
+      'width': '200px'
+    }), TweenMax.to($('.culture'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.how_we_do_it .black_block.b4'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.culture', 0.5, {})).add([
+    TweenMax.to($('.culture'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.how_we_do_it .black_block.b4'), 0.15, {
+      'opacity': '0.0001'
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln6'), 0.2, {
+      'width': '440px'
+    }), TweenMax.fromTo('.communication', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.communication'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.how_we_do_it .black_block.b5'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.culture', 0.5, {})).add([
+    TweenMax.to($('.guidelines .ln6'), 0.2, {
+      'width': '460px'
+    }), TweenMax.to($('.communication'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.how_we_do_it .black_block.b5'), 0.15, {
+      'opacity': '0.0001'
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln6'), 0.2, {
+      'width': '680px'
+    }), TweenMax.fromTo('.content', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln6'), 0.5, {
+      'width': '750px'
+    }), TweenMax.to($('.content'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.how_we_do_it .black_block.b6'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.culture', 0.5, {})).add([
+    TweenMax.to($('.content'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.how_we_do_it .black_block.b6'), 0.05, {
+      'opacity': '0.0001'
+    })
+  ]).add(TweenMax.to($('.guidelines .ln6'), 0.1, {
+    'width': '940px'
+  }));
+  scene_what_we_do_tips = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 1300,
+    offset: 4400,
+    tweenChanges: true
+  }).triggerHook(0.3).setTween(how_we_do_it_icons_tween).setPin('.about_us .container.long').addTo(controller);
+  jessica_tween2 = new TimelineMax().add([
+    TweenMax.to($('.guidelines .ln7'), 0.1, {
+      'height': '550px',
+      delay: 0.4
+    }), TweenMax.to($('.stories2 .story1'), 4, {
+      'transform': 'translate3D(0,-900px,0)'
+    }), TweenMax.to($('.stories2 .story2'), 2.5, {
+      'transform': 'translate3D(0,-900px,0)'
+    }), TweenMax.to($('.stories2 .story3'), 2, {
+      'transform': 'translate3D(0,-1100px,0)',
+      delay: 0.1
+    }), TweenMax.to($('.stories2 .story4'), 3, {
+      'transform': 'translate3D(0,-1100px,0)',
+      delay: 0.3
+    }), TweenMax.to($('.stories2 .story5'), 3.5, {
+      'transform': 'translate3D(0,-1200px,0)',
+      delay: 0.6
+    }), TweenMax.to($('.stories2 .story1 .quote'), 4.5, {
+      'transform': 'translate3D(0,-50px,0)'
+    }), TweenMax.to($('.stories2 .story2 .quote'), 5, {
+      'transform': 'translate3D(0,-200px,0)'
+    }), TweenMax.to($('.stories2 .story3 .quote'), 5, {
+      'transform': 'translate3D(0, 100px,0)',
+      delay: 0.1
+    }), TweenMax.to($('.stories2 .story4 .quote'), 4.3, {
+      'transform': 'translate3D(0,-250px,0)',
+      delay: 0.3
+    }), TweenMax.to($('.stories2 .story5 .quote'), 4.8, {
+      'transform': 'translate3D(0,-200px,0)',
+      delay: 0.6
+    }), TweenMax.to($('.guidelines .ln8'), 2, {
+      'height': '1032px',
+      delay: 1
+    })
+  ]).add(TweenMax.to($('.guidelines .ln8_1'), 0.2, {
+    'width': '940px'
+  }));
+  scene_jessica2 = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 1100,
+    offset: 5900
+  }).triggerHook(0.3).on('start', function(e) {
+    if (e.target.controller().info("scrollDirection") === "FORWARD") {
+      return $('.ln6').addClass('complete');
+    } else {
+      return $('.ln6').removeClass('complete');
+    }
+  }).setTween(jessica_tween2).addTo(controller);
+  return $('.iconset .item').click(function() {
+    var id;
+    $('.active_icon').removeClass('active_icon');
+    $('.iconset .item').addClass('disabled');
+    $('.iconset .black_block').addClass('disabled');
+    id = $(this).data('id');
+    $(this).addClass('active_icon').removeClass('disabled');
+    return $('.' + id).addClass('active_icon').removeClass('disabled');
+  });
+};
+});
+
+;require.register("scrollmagic/about_section_ipad", function(exports, require, module) {
+module.exports = function(controller) {
+  var about_us_tween, how_we_do_it_icons_tween, how_we_do_it_tween, jessica_tween, jessica_tween2, scene_about_us, scene_how_we_do_it, scene_jessica, scene_jessica2, scene_what_we_do_text, scene_what_we_do_tips, scene_who_we_are, scene_who_we_are_text, what_we_do_tips_tween, who_we_are_tween;
+  about_us_tween = new TimelineMax().add(TweenMax.to($('.about_us .video_section_about_us .label'), 1, {
+    'transform': 'translate3D(0,-250px,0)'
+  }));
+  scene_about_us = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    offset: 0,
+    duration: '200%'
+  }).triggerHook(1).setTween(about_us_tween).addTo(controller);
+  who_we_are_tween = new TimelineMax().add(TweenMax.to($('.guidelines .ln1'), 2, {
+    'height': '750px'
+  }));
+  scene_who_we_are = new ScrollMagic.Scene({
+    triggerElement: ".about_us .container",
+    duration: '600px'
+  }).triggerHook(0.5).setTween(who_we_are_tween).addTo(controller);
+  scene_who_we_are_text = new ScrollMagic.Scene({
+    triggerElement: ".about_us .who"
+  }).triggerHook(0.6).on("start", function() {
+    return $('.about_us .who .h1, .about_us .who .col').addClass('showed');
+  }).addTo(controller);
+  scene_what_we_do_text = new ScrollMagic.Scene({
+    triggerElement: ".about_us .h2",
+    offset: 0
+  }).triggerHook(0.3).on("start", function() {
+    return $('.about_us .h2').addClass('showed');
+  }).addTo(controller);
+  what_we_do_tips_tween = new TimelineMax().add([
+    TweenMax.to($('.guidelines .ln2'), 0.2, {
+      'width': '130px'
+    }), TweenMax.fromTo('.insights', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln2'), 0.5, {
+      'width': '200px'
+    }), TweenMax.to($('.insights'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.what_we_do .black_block.b1'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.insights', 0.5, {})).add([
+    TweenMax.to($('.insights'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.what_we_do .black_block.b1'), 0.15, {
+      'opacity': '0.0001'
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln2'), 0.2, {
+      'width': '440px'
+    }), TweenMax.fromTo('.strategy', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.strategy'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.what_we_do .black_block.b2'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.insights', 0.5, {})).add([
+    TweenMax.to($('.guidelines .ln2'), 0.2, {
+      'width': '460px'
+    }), TweenMax.to($('.strategy'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.what_we_do .black_block.b2'), 0.15, {
+      'opacity': '0.0001'
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln2'), 0.2, {
+      'width': '680px'
+    }), TweenMax.fromTo('.execution', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln2'), 0.5, {
+      'width': '750px'
+    }), TweenMax.to($('.execution'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.what_we_do .black_block.b3'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.insights', 0.5, {})).add([
+    TweenMax.to($('.execution'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.what_we_do .black_block.b3'), 0.05, {
+      'opacity': '0.0001'
+    })
+  ]).add(TweenMax.to($('.guidelines .ln2'), 0.1, {
+    'width': '940px'
+  }));
+  scene_what_we_do_tips = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
     duration: 350,
     offset: 900,
     tweenChanges: true
@@ -511,7 +825,7 @@ module.exports = function(controller) {
       delay: 0.6
     }), TweenMax.to($('.guidelines .ln4'), 2, {
       'height': '1132px',
-      delay: 0.6
+      delay: 1
     })
   ]).add(TweenMax.to($('.guidelines .ln4_1'), 0.2, {
     'width': '940px'
@@ -650,7 +964,7 @@ module.exports = function(controller) {
       delay: 0.6
     }), TweenMax.to($('.guidelines .ln8'), 2, {
       'height': '1032px',
-      delay: 0.6
+      delay: 1
     })
   ]).add(TweenMax.to($('.guidelines .ln8_1'), 0.2, {
     'width': '940px'
@@ -772,16 +1086,14 @@ module.exports = function(controller) {
     'transform': 'translate3D(0,-250px,0)'
   }));
   scene_get_smarter = new ScrollMagic.Scene({
-    triggerElement: ".about_us",
-    offset: 5660,
+    triggerElement: ".get_smarter_video",
     duration: '200%'
   }).triggerHook(1).setTween(get_smarter_tween).addTo(controller);
   contact_us_video_tween = new TimelineMax().add(TweenMax.to($('.contact_us_video .label'), 1, {
     'transform': 'translate3D(0,-250px,0)'
   }));
   scene_get_smarter = new ScrollMagic.Scene({
-    triggerElement: ".about_us",
-    offset: 6660,
+    triggerElement: ".contact_us_video",
     duration: '200%'
   }).triggerHook(1).setTween(contact_us_video_tween).addTo(controller);
   $('.purchase_form svg path').each(function(i, e) {
@@ -800,6 +1112,118 @@ module.exports = function(controller) {
 });
 
 ;require.register("scrollmagic/get_started_section", function(exports, require, module) {
+module.exports = function(controller) {
+  var end_of_guideline_tween, get_started_tween, scene_end_of_guideline, scene_get_started, scene_get_started_tips;
+  get_started_tween = new TimelineMax().add(TweenMax.to($('.guidelines .ln9'), 2, {
+    'height': '330px'
+  })).add(TweenMax.to($('.get_started.fadein'), 1, {
+    'opacity': '0.9999'
+  }));
+  scene_get_started = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 200,
+    offset: 7000
+  }).triggerHook(0.2).setTween(get_started_tween).addTo(controller);
+  get_started_tween = new TimelineMax().add([
+    TweenMax.to($('.guidelines .ln10'), 0.2, {
+      'width': '130px'
+    }), TweenMax.fromTo('.assessments', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln10'), 0.5, {
+      'width': '200px'
+    }), TweenMax.to($('.assessments'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.get_started_iconset .black_block.b7'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.assessments', 0.5, {})).add([
+    TweenMax.to($('.assessments'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.get_started_iconset .black_block.b7'), 0.15, {
+      'opacity': '0.0001'
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln10'), 0.2, {
+      'width': '440px'
+    }), TweenMax.fromTo('.workshops', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.workshops'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.get_started_iconset .black_block.b8'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.assessments', 0.5, {})).add([
+    TweenMax.to($('.guidelines .ln10'), 0.2, {
+      'width': '460px'
+    }), TweenMax.to($('.workshops'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.get_started_iconset .black_block.b8'), 0.15, {
+      'opacity': '0.0001'
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln10'), 0.2, {
+      'width': '680px'
+    }), TweenMax.fromTo('.speaking', 0.2, {
+      opacity: '0.001'
+    }, {
+      opacity: '0.9999',
+      delay: 0.1
+    })
+  ]).add([
+    TweenMax.to($('.guidelines .ln10'), 0.5, {
+      'width': '750px'
+    }), TweenMax.to($('.speaking'), 0.5, {
+      'transform': 'scale(1.3)',
+      'color': '#000'
+    }), TweenMax.to($('.get_started_iconset .black_block.b9'), 0.2, {
+      'opacity': '0.9999'
+    })
+  ]).add(TweenMax.to('.assessments', 0.5, {})).add([
+    TweenMax.to($('.speaking'), 0.2, {
+      'transform': 'scale(1)'
+    }), TweenMax.to($('.get_started_iconset .black_block.b9'), 0.05, {
+      'opacity': '0.0001'
+    })
+  ]).add(TweenMax.to($('.guidelines .ln10'), 1, {
+    'width': '940px'
+  }));
+  scene_get_started_tips = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 1350,
+    offset: 7450,
+    tweenChanges: true
+  }).triggerHook(0.1).setPin('.about_us .container.long').setTween(get_started_tween).addTo(controller);
+  end_of_guideline_tween = new TimelineMax().add(TweenMax.to($('.guidelines .ln11'), 2, {
+    'height': '550px',
+    delay: 0.6
+  }));
+  return scene_end_of_guideline = new ScrollMagic.Scene({
+    triggerElement: ".about_us",
+    duration: 200,
+    offset: 8801
+  }).triggerHook(0.2).on('start', function(e) {
+    if (e.target.controller().info("scrollDirection") === "FORWARD") {
+      return $('.ln10').addClass('complete');
+    } else {
+      return $('.ln10').removeClass('complete');
+    }
+  }).setTween(end_of_guideline_tween).addTo(controller);
+};
+});
+
+;require.register("scrollmagic/get_started_section_ipad", function(exports, require, module) {
 module.exports = function(controller) {
   var end_of_guideline_tween, get_started_tween, scene_end_of_guideline, scene_get_started, scene_get_started_tips;
   get_started_tween = new TimelineMax().add(TweenMax.to($('.guidelines .ln9'), 2, {
@@ -895,7 +1319,7 @@ module.exports = function(controller) {
   }).triggerHook(0.3).setTween(get_started_tween).addTo(controller);
   end_of_guideline_tween = new TimelineMax().add(TweenMax.to($('.guidelines .ln11'), 2, {
     'height': '550px',
-    delay: 0.4
+    delay: 0.6
   }));
   return scene_end_of_guideline = new ScrollMagic.Scene({
     triggerElement: ".about_us",
